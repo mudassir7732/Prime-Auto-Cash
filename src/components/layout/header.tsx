0c9d245110ab48d5b0d2dfb8871ee68cf9e70238
mudassir7732 <mudassir7732@gmail.com>
2024-09-13 20:38:27 +0500

    Added new dependencies

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,11 +2,34 @@ import { FC } from "react";
 
 const Header: FC = () => {
     return (
-        <div className="bg-black w-full h-[102px] pt-[35px] pb-[25px]">
-            <p className="">
-                Header
+        <div className="bg-black w-full h-[102px] pt-[35px] pb-[25px] px-[78px] flex flex-row items-center justify-between">
+            <p className="font-podium-sharp font-[800] text-white text-[24px] ">
+                PRIMEAUTOCASH
             </p>
-        </div>
+            <div className="flex flex-row items-center justify-between gap-x-[64px]">
+                <div className="flex flex-row items-center justify-betweens gap-x-[9.8px]">
+                    <div className="flex flex-row items-center justify-between gap-x-[11.8px]">
+                        <img src="/assets/icons/insta.svg" className="h-[20px] w-[20px] cursor-pointer" />
+                        <img src="/assets/icons/fb.svg" className="h-[20px] w-[20px] cursor-pointer" />
+                        <img src="/assets/icons/dot.svg" className="h-[4px] w-[4px]" />
+                    </div>
+                    <p className="text-[11px] text-white font-[Poppins] font-[400]">
+                        +1 (682) 222-2242
+                    </p>
+                </div>
+                <div className="flex flex-row items-center justify-between gap-x-[28px]">
+                    <button className="text-[11px] font-[400] font-[Poppins] text-white">
+                        ABOUT US
+                    </button>
+                    <button className="text-[11px] font-[400] font-[Poppins] text-white">
+                        CONTACT US
+                    </button>
+                    <button className="text-[11px] font-[400] font-[Poppins] text-white">
+                        FAQ
+                    </button>
+                </div>
+            </div>
+        </div >
     )
 }
 export default Header;
