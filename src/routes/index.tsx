61079dddcf412146a9977fed05c3572b1932771d
mudassir7732 <mudassir7732@gmail.com>
2024-09-13 20:00:39 +0500

    Added new dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home";
+import Information from "../pages/information";
 
 
 const AppRoutes:FC=()=>{
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={} />
+                <Route path="/" element={<Home />} />
+                <Route path="/information" element={<Information />} />
             </Routes>
         </BrowserRouter>
     )
