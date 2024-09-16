3bc13fb20a44658a25f8b202fb52b8a8a7b6b67a
mudassir7732 <mudassir7732@gmail.com>
2024-09-16 20:02:21 +0500

    Updated Code

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,27 +2,27 @@ import { FC } from "react";
 
 const Footer: FC = () => {
     return (
-        <div className=" bg-[#f7f7f7] flex flex-col items-center pt-[58px] mt-0">
-            <p className="font-podium-sharp font-[800] text-black text-[43px] mb-[40px]">
+        <div className=" bg-[#f7f7f7] flex flex-col items-center footer mt-0">
+            <p className="font-podium-sharp font-[800] text-black footer-logo">
                 PRIMEAUTOCASH
             </p>
-            <div className="flex flex-row items-center gap-x-[68px] mb-[63px]">
-                <button className="text-[22.5px] text-black font-[400] font-sf-pro">
+            <div className="flex flex-row items-center footer-buttons">
+                <button className="footer-button text-black font-[400] font-sf-pro">
                     Home
                 </button>
-                <button className="text-[22.5px] text-black font-[400] font-sf-pro">
+                <button className="footer-button text-black font-[400] font-sf-pro">
                     FAQ
                 </button>
-                <button className="text-[22.5px] text-black font-[400] font-sf-pro">
+                <button className="footer-button text-black font-[400] font-sf-pro">
                     Terms & Condition
                 </button>
-                <button className="text-[22.5px] text-black font-[400] font-sf-pro">
+                <button className="footer-button text-black font-[400] font-sf-pro">
                     Privacy Policy
                 </button>
             </div>
-            <div className="h-[52px] w-full flex flex-row items-center">
+            <div className="footer-bottom-bar w-full flex flex-row items-center">
                 <div className="w-full bg-black h-full rounded-tr-full" />
-                <div className="min-w-[266px] flex flex-row items-center justify-center text-[14px] font-[400] font-sf-pro text-black">
+                <div className="footer-bottom-text flex flex-row items-center justify-center font-[400] font-sf-pro text-black">
                     © 2024
                     <p className="ml-1 underline">
                         MK AUTOS
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,28 +3,28 @@ import { FC } from "react";
 const Header: FC = () => {
     return (
         <div className="bg-black w-full header pt-[35px] pb-[25px] px-[78px] flex flex-row items-center justify-between">
-            <p className="font-podium-sharp font-[800] text-white text-[24px] ">
+            <p className="font-podium-sharp font-[800] text-white header-logo">
                 PRIMEAUTOCASH
             </p>
-            <div className="flex flex-row items-center justify-between gap-x-[64px]">
-                <div className="flex flex-row items-center justify-betweens gap-x-[9.8px]">
-                    <div className="flex flex-row items-center justify-between gap-x-[11.8px]">
-                        <img src="/assets/icons/insta.svg" className="h-[20px] w-[20px] cursor-pointer" />
-                        <img src="/assets/icons/fb.svg" className="h-[20px] w-[20px] cursor-pointer" />
+            <div className="flex flex-row items-center justify-between header-buttons-container">
+                <div className="flex flex-row items-center justify-betweens header-icons-wrapper">
+                    <div className="flex flex-row items-center justify-between header-icons">
+                        <img src="/assets/icons/insta.svg" className="header-icon cursor-pointer" />
+                        <img src="/assets/icons/fb.svg" className="header-icon cursor-pointer" />
                         <img src="/assets/icons/dot.svg" className="h-[4px] w-[4px]" />
                     </div>
-                    <p className="text-[11px] text-white font-[Poppins] font-[400]">
+                    <p className="header-button underline text-white font-[Poppins] font-[400]">
                         +1 (682) 222-2242
                     </p>
                 </div>
-                <div className="flex flex-row items-center justify-between gap-x-[28px]">
-                    <button className="text-[11px] font-[400] font-[Poppins] text-white outline-none">
+                <div className="flex flex-row items-center justify-between header-buttons-wrapper">
+                    <button className="header-button font-[400] font-[Poppins] text-white outline-none">
                         ABOUT US
                     </button>
-                    <button className="text-[11px] font-[400] font-[Poppins] text-white outline-none">
+                    <button className="header-button font-[400] font-[Poppins] text-white outline-none">
                         CONTACT US
                     </button>
-                    <button className="text-[11px] font-[400] font-[Poppins] text-white outline-none">
+                    <button className="header-button font-[400] font-[Poppins] text-white outline-none">
                         FAQ
                     </button>
                 </div>
