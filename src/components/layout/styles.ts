15093e3db31ab463a2d50f760c233273f3286cc9
mudassir7732 <mudassir7732@gmail.com>
2024-09-17 15:56:31 +0500

    Updated Styles

---
+++ src/components/layout/styles.ts
@@ -0,0 +1,4 @@
+const styles = {
+    header_container:"bg-black w-full xl:h-[72px] 2xl:h-[102px] pt-[35px] pb-[25px] px-[78px] flex flex-row items-center justify-between",
+}
+export default styles;
