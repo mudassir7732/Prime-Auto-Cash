14452891cfba4f8c334fcc45cbceec0b5aab5ac8
mudassir7732 <mudassir7732@gmail.com>
2024-09-17 17:43:00 +0500

    Updated Styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import Layout from "../components/layout";
 import Information1 from "../pages/information1";
@@ -7,12 +7,12 @@ import Information1 from "../pages/information1";
 
 const AppRoutes: FC = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<Layout><Home /></Layout>} />
                 <Route path="/information1" element={<Layout><Information1 /></Layout>} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
 export default AppRoutes;
